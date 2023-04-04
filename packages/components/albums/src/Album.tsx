e969a027ca85efef9ce1ce646b7251fa6615c2cc
sleepyfran <fgzv@outlook.com>
2023-04-05 01:21:33 +0200

    :lipstick: Improve styles and add hover state

--- packages/components/albums/src/Album.tsx
+++ packages/components/albums/src/Album.tsx
@@ -1,21 +1,35 @@
-import { Flex, Image, Text } from "@mantine/core";
+import { Flex, Image, Text, createStyles } from "@mantine/core";
 import { SimpleAlbum } from "@spotless/types";
 
 type AlbumProps = {
   album: SimpleAlbum;
 };
 
+const useStyles = createStyles((theme) => ({
+  card: {
+    padding: theme.spacing.sm,
+    ":hover": {
+      backgroundColor: theme.colors.gray[9],
+      borderRadius: theme.radius.md,
+    },
+  },
+}));
+
 /**
  * Component that displays an album, including its name, artist name and cover image.
  */
-export const Album = ({ album }: AlbumProps) => (
-  <Flex direction="column">
-    <Image src={album.coverUrl} width={150} height={150} radius="sm" />
-    <Text fz="lg" variant="gradient" truncate>
-      {album.name}
-    </Text>
-    <Text fz="sm" c="dimmed" truncate>
-      {album.artistName}
-    </Text>
-  </Flex>
-);
+export const AlbumCard = ({ album }: AlbumProps) => {
+  const styles = useStyles();
+
+  return (
+    <Flex direction="column" align="center" className={styles.classes.card}>
+      <Image src={album.coverUrl} width={150} height={150} radius="sm" />
+      <Text fz="lg" variant="gradient" lineClamp={1}>
+        {album.name}
+      </Text>
+      <Text fz="sm" c="dimmed" lineClamp={1}>
+        {album.artistName}
+      </Text>
+    </Flex>
+  );
+};
--- packages/components/home/src/Home.tsx
+++ packages/components/home/src/Home.tsx
@@ -1,28 +1,35 @@
-import { Flex, Loader, Grid, Title } from "@mantine/core";
-import { Album } from "@spotless/components-albums";
+import { Flex, Loader, Grid, Title, useMantineTheme } from "@mantine/core";
+import { AlbumCard } from "@spotless/components-albums";
 import { useServices } from "@spotless/components-shared";
 import { useQuery } from "@tanstack/react-query";
 
 export const Home = () => {
+  const { spacing } = useMantineTheme();
   const { albumsService } = useServices();
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isFetching } = useQuery({
     queryKey: ["home-albums"],
     queryFn: () => albumsService.home(),
   });
 
   return (
     <Flex direction="column" gap={10}>
-      <Title>Home</Title>
-      <Title order={5}>Last 50 albums</Title>
-      {isLoading && <Loader size="xl" />}
-      <Grid>
-        {data?.map((album) => (
-          <Grid.Col key={album.id} xs={4} sm={3} md={2} xl={1}>
-            <Album album={album} />
-          </Grid.Col>
-        ))}
-      </Grid>
+      <Flex direction="column" pl={spacing.md}>
+        <Flex align="center" gap={10}>
+          <Title>Home</Title>
+          {(isLoading || isFetching) && <Loader size="sm" />}
+        </Flex>
+        <Title order={5}>Last 50 albums</Title>
+      </Flex>
+      {data && (
+        <Grid>
+          {data?.map((album) => (
+            <Grid.Col key={album.id} xs={4} sm={3} md={2} xl={1}>
+              <AlbumCard album={album} />
+            </Grid.Col>
+          ))}
+        </Grid>
+      )}
     </Flex>
   );
 };
