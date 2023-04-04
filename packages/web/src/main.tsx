115fcc907899d5c24980a6da7171cab142bde965
sleepyfran <fgzv@outlook.com>
2023-04-04 16:41:28 +0200

    :goal_net: Show on startup when env vars are missing

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -8,10 +8,21 @@ import {
 } from "@spotless/components-shared";
 
 const init = async () => {
+  const baseUrl = import.meta.env.VITE_BASE_APP;
+  const clientId = import.meta.env.VITE_CLIENT_ID;
+  const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+  if (!baseUrl || !clientId || !clientSecret) {
+    document.body.innerHTML = "<h1>Missing environment variables</h1>";
+    throw new Error(
+      "Missing environment variables. Please make sure there is an .env file for this environment with a VITE_BASE_APP, VITE_CLIENT_ID and VITE_CLIENT_SECRET variable."
+    );
+  }
+
   const services = await initializeServices({
-    baseUrl: import.meta.env.VITE_BASE_APP,
-    clientId: import.meta.env.VITE_CLIENT_ID,
-    clientSecret: import.meta.env.VITE_CLIENT_SECRET,
+    baseUrl,
+    clientId,
+    clientSecret,
   });
 
   const root = createRoot(document.getElementById("root") as HTMLElement);
