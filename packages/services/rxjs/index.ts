a6da3d2936fc03024937e38bca7df9ce45f525d2
sleepyfran <fgzv@outlook.com>
2023-04-10 23:34:50 +0200

    :fire: Remove unused utils

--- packages/services/rxjs/index.ts
+++
@@ -1,8 +0,0 @@
-import { Observable, lastValueFrom, toArray } from "rxjs";
-
-/**
- * Collects all the values emitted by the source observable into a promise that
- * resolves an array of all these values.
- */
-export const collectIntoArray = <T>(source: Observable<T>): Promise<T[]> =>
-  lastValueFrom(source.pipe(toArray()));
